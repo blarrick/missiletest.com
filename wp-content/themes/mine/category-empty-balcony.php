3d1da54e092529b7791dc2ad5b61e1388ef21305
Bryan Larrick <blarrick@bryanlarrick.com>
2023-10-13 09:52:47 -0400

    add deadly manor

--- wp-content/themes/mine/category-empty-balcony.php
+++ wp-content/themes/mine/category-empty-balcony.php
@@ -342,6 +342,7 @@ get_header(); ?>
 				<li><a href="/2018/10/11/dead-pit/">Dead Pit, The</a></li>
 				<li><a href="/2016/10/03/dead-silence/">Dead Silence</a></li>
 				<li class="shitty"><a href="/2019/10/16/dead-trigger/">Dead Trigger</a></li>
+				<li class="shitty"><a href="/2023/10/12/deadly-manor/">Deadly Manor</a></li>
 				<li class="shitty"><a href="/2018/10/08/deadly-mantis/">Deadly Mantis, The</a></li>
 				<li class="shitty"><a href="/2020/07/05/deadly-prey/">Deadly Prey</a></li>
 				<li class="shitty"><a href="/2022/10/06/deadly-spawn/">Deadly Spawn, The</a></li>
--- wp-content/themes/mine/category-shitty-movie-sundays.php
+++ wp-content/themes/mine/category-shitty-movie-sundays.php
@@ -370,6 +370,7 @@ get_header(); ?>
 				<li class="has-image sleepaway-camp" onclick="location.href='/2014/10/29/sleepaway-camp/';"><a href="/2014/10/29/sleepaway-camp/">Sleepaway Camp</a></li>
 				<li class="has-image psycho-pike" onclick="location.href='/2022/10/13/psycho-pike/';"><a href="/2022/10/13/psycho-pike/">Psycho Pike</a></li>
 				<li class="has-image ozone" onclick="location.href='/2022/10/29/ozone/';"><a href="/2022/10/29/ozone/">Ozone</a></li>
+				<li class="has-image deadly-manor" onclick="location.href='/2023/10/12/deadly-manor/';"><a href="/2023/10/12/deadly-manor/">Deadly Manor</a></li>
 				<li class="has-image french-quarter" onclick="location.href='/2023/08/13/french-quarter/';"><a href="/2023/08/13/french-quarter/">French Quarter</a></li>
 				<li class="has-image double-exposure" onclick="location.href='/2020/02/09/double-exposure/';"><a href="/2020/02/09/double-exposure/">Double Exposure</a></li>
 				<li class="has-image naked-angels" onclick="location.href='/2021/04/04/naked-angels/';"><a href="/2021/04/04/naked-angels/">Naked Angels</a></li>
