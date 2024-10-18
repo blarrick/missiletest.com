02d02fbbed111235430dba3877e21cf054f838e5
Bryan Larrick <blarrick@bryanlarrick.com>
2024-10-18 10:46:11 -0400

    add creeping flesh

--- wp-content/themes/mine/category-empty-balcony.php
+++ wp-content/themes/mine/category-empty-balcony.php
@@ -329,6 +329,7 @@ get_header(); ?>
 				<li class="shitty"><a href="/2019/10/14/creature-walks-among-us/">Creature Walks Among Us, The</a></li>
 				<li><a href="/2017/08/31/creed/">Creed</a></li>
 				<li><a href="/2019/10/20/creep-2004/">Creep</a> <span>(2004)</span></li>
+				<li><a href="/2024/10/17/creeping-flesh/">Creeping Flesh, The</a></li>
 				<li class="shitty"><a href="/2022/10/24/creeping-terror/">Creeping Terror, The</a></li>
 				<li><a href="/2017/10/02/quatermass-xperiment/">Creeping Unknown, The, aka The Quatermass Xperiment</a></li>
 				<li class="shitty"><a href="/2023/10/19/creepozoids/">Creepozoids</a></li>
