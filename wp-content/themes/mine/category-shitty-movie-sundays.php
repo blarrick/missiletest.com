1cc25212ff9894560a60d2b5e61a2639a1fee0d0
Bryan Larrick <blarrick@bryanlarrick.com>
2025-10-21 10:02:13 -0400

    add killer crocodile

--- wp-content/themes/mine/category-shitty-movie-sundays.php
+++ wp-content/themes/mine/category-shitty-movie-sundays.php
@@ -192,6 +192,7 @@ get_header(); ?>
 				<li class="has-image childs-play-3" onclick="location.href='/2021/10/15/childs-play-3/';"><a href="/2021/10/15/childs-play-3/">Child’s Play 3</a></li>
 				<li class="has-image nine-deaths" onclick="location.href='/2022/07/31/nine-deaths-of-the-ninja/';"><a href="/2022/07/31/nine-deaths-of-the-ninja/">Nine Deaths of the Ninja</a></li>
 				<li class="has-image future-zone" onclick="location.href='/2023/12/10/future-zone/';"><a href="/2023/12/10/future-zone/">Future Zone</a></li>
+				<li class="has-image killer-crocodile" onclick="location.href='/2025/10/21/killer-crocodile/';"><a href="/2025/10/21/killer-crocodile/">Killer Crocodile</a></li>
 				<li class="has-image squirm" onclick="location.href='/2020/10/02/squirm/';"><a href="/2020/10/02/squirm/">Squirm</a></li>
 				<li class="has-image mutilator" onclick="location.href='/2023/10/08/mutilator/';"><a href="/2023/10/08/mutilator/">The Mutilator</a></li>
 				<li class="has-image things" onclick="location.href='/2022/10/14/things/';"><a href="/2022/10/14/things/">Things</a></li>
