4cb051fd2bac5a5f5b5e88a4ec2dea30c7e91b91
blarrick <blarrick@bryanlarrick.com>
2026-05-04 15:46:41 -0400

    undo test change

--- wp-content/themes/mine/header.php
+++ wp-content/themes/mine/header.php
@@ -38,7 +38,7 @@
 					<?php twentysixteen_the_custom_logo(); ?>
 
 					<?php if ( is_front_page() && is_home() ) : ?>
-						<h1 class="site-title">nbn<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 					<?php else : ?>
 						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 					<?php endif;
